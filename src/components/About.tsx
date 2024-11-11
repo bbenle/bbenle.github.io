7ce1e356ea97fb8db54bd1f28913bb4a69d508ac
bbenle <bbenle@hotmail.com>
2024-11-11 14:31:15 +0800

    progress

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./About.css";
 
+
 const About: React.FC = () => {
     return (
         <div className="about">
-            Hi, My Name is Benjamin Le
+            Hello, my name is Benjamin Le
         </div>
     )
 }
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,9 +3,8 @@ import "./NavigationBar.css";
 
 const NavigationBar: React.FC = () => {
     return (
-        <div className="navbar">
-            <a>Home</a>
-            <a>About</a>
+        <div className="nav-bar">
+            <a>About Me</a>
             <a>Experience</a>
             <a>Projects</a>
             <a>Contact</a>
