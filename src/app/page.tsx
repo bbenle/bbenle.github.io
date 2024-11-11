855badd189496e87c03d28a645e5fd2ead2bb2fb
bbenle <bbenle@hotmail.com>
2024-11-11 10:28:45 +0800

    app folder fix

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import NavigationBar from "../components/NavigationBar";
+import About from "../components/About";
+import Experience from "../components/Experience";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+
+export default function Page() {
+  return (
+    <div>
+      <NavigationBar />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
