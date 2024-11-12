be42dc9db34505d88d23b55bfc1c6173f03a2a09
bbenle <bbenle@hotmail.com>
2024-11-12 15:00:30 +0800

    more progress on experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -19,25 +19,25 @@ const Experience: React.FC = () => {
                     company="Capgemini"
                     title="Consultant / Software Engineer"
                     image="/images/Capgemini.png"
-                    description="Worked at Company X developing features in Java and contributing to the backend."
+                    description="I worked as a Consultant and Software Engineer at Capgemini, mostly working on internal projects including a full-stack user project tracking application, pit-to-port command centre proof of concept, and Australian Engineering Week"
                 />
                 <InfoBox
                     company="Imdex Limited"
                     title="Graduate Software Engineer"
                     image="/images/Imdex.jpeg"
-                    description="Worked at Company X developing features in Java and contributing to the backend."
+                    description="I collaborated on frontend development using Angular for Imdex HUB-IQ projects. This included unit testing, automated testing, refactoring code, collaborating with cross-functional teams such as UI/UX designers, product managers etc."
                 />
                 <InfoBox
                     company="Curtin University"
                     title="Sessional Academic"
                     image="/images/Curtin.jpg"
-                    description="Worked at Company X developing features in Java and contributing to the backend."
+                    description="I tutored two units - Introduction to Software Engineering (ISAD1000) and Fundamentals of Programming (COMP1005)."
                 />
                 <InfoBox
                     company="H&M"
                     title="Sales Advisor"
                     image="/images/Hm.png"
-                    description="Worked at Company X developing features in Java and contributing to the backend."
+                    description="I worked at H&M as a sales advisor. My job was to provide the best customer service possible and to work together with a team that has the drive and ambition to reach business goals"
                 />
             </div>
         </div>
