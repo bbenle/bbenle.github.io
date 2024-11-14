9b6c49b7010eecb3981e9b40d12bcc79fc23cf9d
bbenle <bbenle@hotmail.com>
2024-11-14 14:24:01 +0800

    created a picture carousel

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -12,31 +12,31 @@ const Experience: React.FC = () => {
                 <InfoBox
                     company="Department of Education"
                     title="Technical Support Assistant"
-                    image="images/Education.jpeg"
+                    image="/images/icons/Education.jpeg"
                     description="I am currently working at the Department of Education on a fixed term contract up until the end of January 2025."
                 />
                 <InfoBox
                     company="Capgemini"
                     title="Consultant / Software Engineer"
-                    image="/images/Capgemini.png"
+                    image="/images/icons/Capgemini.png"
                     description="I worked as a Consultant and Software Engineer, mostly working on internal projects including a full-stack user project tracking app, pit-to-port command centre proof of concept, and Australian Engineering Week demos."
                 />
                 <InfoBox
                     company="Imdex Limited"
                     title="Graduate Software Engineer"
-                    image="/images/Imdex.jpeg"
+                    image="/images/icons/Imdex.jpeg"
                     description="I collaborated on frontend development for Imdex HUB-IQ projects using Angular. This included unit testing, code refactoring, and working closely with cross-functional teams such as UI/UX designers and product managers."
                 />
                 <InfoBox
                     company="Curtin University"
                     title="Sessional Academic"
-                    image="/images/Curtin.jpg"
+                    image="/images/icons/Curtin.jpg"
                     description="I tutored two units - Introduction to Software Engineering (ISAD1000) and Fundamentals of Programming (COMP1005). I organised and facilitated classes of up to 15 first-year university students."
                 />
                 <InfoBox
                     company="H&M"
                     title="Sales Advisor"
-                    image="/images/Hm.png"
+                    image="/images/icons/Hm.png"
                     description="I worked at H&M as a sales advisor. My job was to provide the best customer service possible and to work together with a team that has the drive and ambition to reach business goals."
                 />
             </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import React from "react";
 import "./Projects.css";
+import ImageSlider from 'simple-image-carousel';
+
+const images = [
+    { url: "images/CapgeminiEngWeek.jpg" },
+    { url: "images/boxing.png" },
+    { url: "images/ImdexCoding.jpg" },
+    { url: "images/ImdexInternship.jpg" },
+
+]
 
 const Projects: React.FC = () => {
     return (
         <div className="projects-container" id="projects">
             <div className="title">
                 Projects
+            </div>
+            <div className="image-slider-container">
+                <ImageSlider objectFit="cover" width={1200} height={700} dotColor="white" dotBorderColor="white" images={images} />
             </div>
         </div>
     )
